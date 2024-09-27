a19d0982e2b5f4ad88b824f4811f8630e7ebb4f7
juangarciasandoval <juan.garciasandoval@telus.com>
2024-09-26 22:27:33 -0600

    first commit

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Next.js Starter kit",
+  description: "A easy way to star projects",
   navItems: [
     {
       label: "Home",
